a46441b7c3c67279385a875235c48b04cc7d549a
Ignacio <ignaciodiaznanni@gmail.com>
2022-03-14 00:13:48 -0300

    controladores terminados

---
+++ api/src/controllers/activities.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const { Country, Activity } = require("../db");
+
+const addActivity = async function (req, res, next) {
+  const { id, name, difficulty, duration, season, addCountries } = req.body;
+  try {
+    const existingActivity = await Activity.findOne({
+      where: {
+        name: name,
+      },
+    });
+    const countriesRelation = await Country.findAll({
+      where: {
+        name: addCountries,
+      },
+    });
+    if (!existingActivity) {
+      const activity = await Activity.create({
+        id,
+        name,
+        difficulty,
+        duration,
+        season,
+      });
+      const relation = await activity.addCountry(countriesRelation);
+      res.status(200).send(relation);
+    } else {
+      const addingCountry = await existingActivity.addCountry(
+        countriesRelation
+      );
+      res.send(addingCountry);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+module.exports = { addActivity };
--- api/src/controllers/countries.js
+++ api/src/controllers/countries.js
@@ -2,12 +2,36 @@ const { Country, Activity } = require("../db");
 const { Op } = require("sequelize");
 
 const getCountries = async function (req, res) {
-    try {
-      const countries = await Country.findAll({ include: Activity });
-      res.status(200).json(countries);
-    } catch (error) {
-      
-      console.log(error);
+    const { name } = req.query;
+
+    if(!name){
+        try {
+        const countries = await Country.findAll({ include: Activity });
+        res.status(200).json(countries);
+        } catch (error) {
+        
+        console.log(error);
+        }
+    }else{
+        try {
+            if (!name) {
+                res.send("No se ingreso ningún nombre");
+              } 
+            const nameCountry = await Country.findAll({
+              where: { name: { [Op.iLike]: `%${name}%` } },
+              include: Activity,
+            });
+            if (nameCountry.length === 0) {
+              res
+                .status(404)
+                .send({ error: `No se encuentra ningún pais con el nombre ${name}` });
+            }
+            res.status(200).send(nameCountry);
+          }
+         catch (e) {
+          console.log("Error in countryByName");
+          console.log(e);
+        } 
     }
   };
   const countryById = async function (req, res) {
@@ -19,6 +43,31 @@ const getCountries = async function (req, res) {
       console.log(error);
     }
   };
+/*   const countryByName = async function (req, res) {
+    const { name } = req.query;
+
+    
+    try {
+        if (!name) {
+            res.send("No se ingreso ningún nombre");
+          } 
+        const nameCountry = await Country.findAll({
+          where: { name: { [Op.iLike]: `%${name}%` } },
+          include: Activity,
+        });
+        if (nameCountry.length === 0) {
+          res
+            .status(404)
+            .send({ error: `No se encuentra ningún pais con el nombre ${name}` });
+        }
+        res.status(200).send(nameCountry);
+      }
+     catch (e) {
+      console.log("Error in countryByName");
+      console.log(e);
+    }
+  }; */
+  
   
 
   module.exports = { getCountries, countryById };
