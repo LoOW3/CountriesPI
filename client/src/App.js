099e733095a31fb1084ee773e73c4e2bb22e5b27
Ignacio <ignaciodiaznanni@gmail.com>
2022-03-15 19:21:21 -0300

    inicio proyecto

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        
+      </Routes>
     </div>
   );
 }
